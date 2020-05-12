4338bb299365f22945919cccc8ffcb45369aa66b
Martijn Arts <m.arts@gmx.net>
2020-05-12 23:01:44 +0200

    Add files

---
+++ src/hello.py
@@ -0,0 +1,12 @@
+import click
+
+
+@click.command()
+@click.option("--who", default="world", help="Whom to greet.")
+def say_hello(who):
+    """ Simple CLI to great someone! """
+    print(f"hello {who}!")
+
+
+if __name__ == "__main__":
+    say_hello()
